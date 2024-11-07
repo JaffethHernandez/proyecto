4648a043041bd4eb8ef66a191def500ce6bc8a5a
JaffethHernandez <44583655+JaffethHernandez@users.noreply.github.com>
2024-11-06 23:11:02 -0600

    version 2

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Categoria,Producto
 
 # Create your views here.
+def index(request):
+    listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
+
+    #print(listaProductos)
+    context = {
+        'productos':listaProductos,
+        'categorias':listaCategorias
+    }
+    return render(request, 'index.html',context)
+
+def productosPorCategoria(request, categoria_id):
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+
+    }
+
+    return render(request, 'index.html', context)
+
+def productosPorNombre(request):
+
+    nombre = request.POST['nombre']
+
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+    listaCategorias = Categoria.objects.all()
+
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+def productoDetalle(request, producto_id):
+
+    #objProducto = Producto.objects.get(pk=producto_id)
+    objProducto = get_object_or_404(Producto, pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+
+    return render (request, 'producto.html', context)
